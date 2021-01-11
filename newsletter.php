c4df38330b6a1c36da28aacc0c840b82a78a9706
Pablo Lires <pablolires@gmail.com>
2021-01-11 18:26:28 -0300

    Implementacion de la pagina del newsletter y estilos (Front)

--- newsletter.php
+++ newsletter.php
@@ -1,28 +1,292 @@
+<?php 
+  include('php/getNewsletters.php');
+?>
+
 <!doctype html>
 <html lang="es">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <!-- Favicons -->
+  <?php include('includes/favicon.inc.php'); ?>
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+  <!-- Normalize -->
+  <link rel="stylesheet" href="node_modules/normalize.css/normalize.css">
 
-    <!-- Optional JavaScript; choose one of the two! -->
+  <!-- Animate -->
+  <link rel="stylesheet" href="node_modules/wowjs/css/libs/animate.css">
 
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css">
 
-    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-    -->
-  </body>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="css/newsletter-front.css">
+
+  <title>Vistage - Newsletter Interno - <?= $newsletter['name'] ?></title>
+
+</head>
+<body>
+
+  <header class="container-fluid">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <h1><img class="img-fluid wow bounceInRight" src="img/logo-vistage.png" alt="logo vistage"></h1>
+        </div>
+      </div>
+    </div>
+    <h2 class="wow bounceInLeft">NEWSLETTER INTERNO <br><span><?= $newsletter['name_month'] .' '. $newsletter['year']  ?></span></h2>
+  </header>
+
+  <section class="container">
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>    
+
+    <!-- Tabla -->
+    <?php if ( !empty($rows_table) ): ?>
+      
+      <div class="tabla">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="wow fadeInLeft">Indicadores Operativos</h2>
+          </div>
+        </div>
+
+        <div class="row wow fadeInLeft">
+          <div class="col-lg-8">
+            <div class="table-responsive">
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col"></th>
+                    <th scope="col text-center">EC</th>
+                    <th scope="col text-center">RP</th>
+                    <th scope="col text-center">CS</th>
+                    <th scope="col text-center">TOTAL</th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                  <?php foreach ($rows_table as $row): ?>
+                    <tr>
+                      <td class="text-center"><?= $row['name'] ?></td>
+                      <td class="text-center"><?= $row['ec'] ?></td>
+                      <td class="text-center"><?= $row['rp'] ?><</td>
+                      <td class="text-center"><?= $row['cs'] ?><</td>
+                      <td class="text-center"><?= $row['total'] ?><</td>
+                    </tr>
+                  <?php endforeach ?>
+                  
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div class="col-lg-4">
+
+            <div class="jumbotron jumbotron-fluid wow fadeInRight">
+              <p class="lead"><?= $newsletter['comment_table_a'] ?></p>
+            </div>
+
+            <div data-wow-delay="0.2s" class="jumbotron jumbotron-fluid wow fadeInRight">
+              <p class="lead"><?= $newsletter['comment_table_b'] ?></p>
+            </div>
+
+            <div data-wow-delay="0.4s" class="jumbotron jumbotron-fluid wow fadeInRight">
+              <p class="lead"><?= $newsletter['comment_table_c'] ?></p>
+            </div>
+            
+            <p class="text-center observaciones wow fadeInUp"><?= $newsletter['observations_table'] ?></p>
+
+          </div>
+        </div>
+      </div>
+      
+    <?php endif ?>
+    <!-- Tabla End -->
+
+    <!-- Calendario -->
+    <?php if ( !empty($calendars) ): ?>
+
+      <div class="calendario">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="wow fadeInLeft">Calendario de Actividades</h2>
+          </div>
+        </div>
+
+        <div class="row wow fadeInUp">
+
+          <?php foreach ($calendars_final as $key => $calendar): ?>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
+
+              <div class="card text-white mb-3" style="max-width: 18rem; margin: auto;">
+                <div class="card-header text-center"><i class="fas fa-calendar-alt"></i><p><?= $calendar['name_day'] .' '. $calendar['number_day'] ?></p></div>
+                <div class="card-body">
+                  <p class="card-text"><?= $calendar['description'] ?></p>
+                </div>
+                <div class="card-footer text-center">
+                  <p><i class="far fa-clock"></i> <?= $calendar['time_init'] .' - '. $calendar['time_end'] ?></p>
+                </div>
+              </div>
+            </div>
+            
+          <?php endforeach ?>
+         
+        </div>
+      </div>
+      
+    <?php endif ?>
+    <!-- Calendario End -->
+
+    <!-- Gerencia General -->
+    <?php if ( !empty($newsletter['gerencia_general']) ): ?>
+
+      <div class="gerencia_general">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="wow fadeInLeft">Gerencia General</h2>
+          </div>
+        </div>
+
+        <div class="row wow fadeInUp">
+          <div class="col-md-12">
+            <p><?= $newsletter['gerencia_general'] ?></p>
+          </div>
+        </div>
+      </div>
+
+    <?php endif ?>
+    <!-- Gerencia General End -->
+
+    <!-- Gerencia y Talento y Negocios -->
+    <?php if ( !empty($newsletter['gerencia_talento']) ): ?>
+
+      <div class="gerencia_talento">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="wow fadeInLeft">Gerencia y Talento y Negocios</h2>
+          </div>
+        </div>
+
+        <div class="row wow fadeInUp">
+          <div class="col-md-12">
+            <p><?= $newsletter['gerencia_talento'] ?></p>
+          </div>
+        </div>
+      </div>
+
+    <?php endif ?>
+    <!-- Gerencia y Talento y Negocios End -->
+
+    <!-- Administración y Finanzas -->
+    <?php if ( !empty($newsletter['administracion_finanzas']) ): ?>
+
+      <div class="gerencia_finanzas">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="wow fadeInLeft">Administración y Finanzas</h2>
+          </div>
+        </div>
+
+        <div class="row wow fadeInUp">
+          <div class="col-md-12">
+            <p><?= $newsletter['administracion_finanzas'] ?></p>
+          </div>
+        </div>
+      </div>
+
+    <?php endif ?>
+    <!-- Administración y Finanzas End -->
+
+    <!-- Marketing -->
+    <?php if ( !empty($newsletter['marketing']) ): ?>
+
+      <div class="marketing">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="wow fadeInLeft">Marketing</h2>
+          </div>
+        </div>
+
+        <div class="row wow fadeInUp">
+          <div class="col-md-12">
+            <p><?= $newsletter['marketing'] ?></p>
+          </div>
+        </div>
+      </div>
+
+    <?php endif ?>
+    <!-- Marketing End -->
+
+    <!-- Operaciones -->
+    <?php if ( !empty($newsletter['operaciones']) ): ?>
+
+      <div class="operaciones">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="wow fadeInLeft">Operaciones</h2>
+          </div>
+        </div>
+
+        <div class="row wow fadeInUp">
+          <div class="col-md-12">
+            <p><?= $newsletter['operaciones'] ?></p>
+          </div>
+        </div>
+      </div>
+
+    <?php endif ?>
+    <!-- Operaciones End -->
+
+  </section>
+
+  <div class="footer-basic container-fluid">
+    <footer class="container">
+
+      <div class="row">
+        <div class="col-md-12">
+          <div class="social">
+            <a target="_blank" class="transition" href="https://www.linkedin.com/company/vistage-s-a-/"><i class="fab fa-linkedin"></i></a>
+            <a target="_blank" class="transition" href="https://www.instagram.com/vistageargentina/"><i class="fab fa-instagram-square"></i></a>
+            <a target="_blank" class="transition" href="https://www.facebook.com/vistage.argentina"><i class="fab fa-facebook-square"></i></a>
+            <a target="_blank" class="transition" href="https://twitter.com/vistageARG"><i class="fab fa-twitter-square"></i></a>
+            <a target="_blank" class="transition" href="https://www.youtube.com/user/VistageArgentina"><i class="fab fa-youtube"></i></a>
+          </div>
+          <div class="text-center">
+            <img class="img-fluid" src="img/logo-footer.png" alt="logo vistage footer">
+          </div>
+        </div>
+      </div>
+
+      <div class="row libre">
+        <div class="col-md-12">
+          <hr>
+          <a target="_blank" href="https://www.librecomunicacion.net/" class="libre transition">by libre</a>
+        </div>  
+      </div>
+
+    </footer>
+  </div>
+
+  <script src="node_modules/jquery/dist/jquery.min.js"></script>
+  <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="node_modules/wowjs/dist/wow.min.js"></script>
+
+  <script>
+    new WOW().init();
+  </script>
+
+</body>
 </html>
---
+++ php/getNewsletters.php
@@ -0,0 +1,73 @@
+<?php
+
+	include('includes/config.inc');
+	include('con.php');
+	require 'vendor/autoload.php';
+
+	if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $sql = "SELECT * FROM newsletters WHERE id = '$id'";
+		$stmt = $db->prepare($sql);
+		$stmt->execute();
+		$newsletter = $stmt->fetch(PDO::FETCH_ASSOC);
+
+		if (!$newsletter) {
+  		header('location: 404.php');
+		}
+
+  } else {
+  	header('location: 404.php');
+  }
+
+	use Carbon\Carbon;
+	carbon::setLocale('es');
+	carbon::setUTF8(false);
+	setlocale(LC_TIME, 'es_ES');
+	date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+	$date = Carbon::now();
+	// var_dump(carbon::setUTF8(true)) ;
+	$currentYear = $date->year;
+
+	$sql = "SELECT * FROM calendars WHERE newsletter_id = '$id'";
+	$stmt = $db->prepare($sql);
+	$stmt->execute();
+	$calendars = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	$sql = "SELECT * FROM tables WHERE newsletter_id = '$id'";
+	$stmt = $db->prepare($sql);
+	$stmt->execute();
+	$rows_table = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	// NEWSLETTER: Agregamos una posicion con el nombre del mes a partir del numero
+	$newsletter['name_month'] = ucfirst(getNameMonth($newsletter, 'localeMonth')); // Agregamos la posicion con el nombre
+
+	$calendars_final = [];
+	foreach ($calendars as $calendar) {
+		// CALENDARIOS: Agregamos diferentes posiciones con el nombre del mes a partir del numero, el numero y nombre del dia
+		$day = getNameDayAndNumber($calendar); // Obtenemos el nombre y numero del dia del evento
+		$calendar['name_month'] = getNameMonth($calendar, 'localeMonth'); // Obtenemos el nombre y numero del dia del evento
+		$calendar['name_day'] = $day['name']; // Agregamos la posicion con el nombre del dia
+		$calendar['number_day'] = $day['number']; // Agregamos la posicion con el numero del dia
+
+		$calendar['time_init'] = substr($calendar['time_init'], 0, -3);
+		$calendar['time_end'] = substr($calendar['time_end'], 0, -3);
+		
+		array_push($calendars_final,$calendar);
+	}
+
+	function getNameMonth($array, $method){
+		$dateCarbon = Carbon::parse('01-' .$array['month']. '-2020'); // Cualquier fecha, solo importa el mes
+		$nameMonth = $dateCarbon->$method;
+		return $nameMonth;
+	}
+
+	function getNameDayAndNumber($array){
+		$day = [];
+		$dateCarbon = Carbon::parse($array['date']); // Creamos el objeto de fecha
+		$day['name'] = $dateCarbon->shortLocaleDayOfWeek;
+		$day['number'] = $dateCarbon->day;
+		return $day;
+	}
+?>
